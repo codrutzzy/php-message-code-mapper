58b04347de0b8baaa886fa08c9b4d5e2b3c4c388
Onofrei Codrut <a.onofrei_extern@ratioform.de>
2025-03-26 23:14:00 +0200

    fix heidel pay

---
+++ lib/Exceptions/MissingLocaleFileException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Heidelpay\MessageCodeMapper\Exceptions;
+
+use Exception;
+
+/**
+ * This class is used for indicating a missing locale file for the library usage.
+ *
+ * @license Use of this software requires acceptance of the License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ *
+ * @link http://dev.heidelpay.com/php-messages-code-mapper
+ *
+ * @author Stephano Vogel <development@heidelpay.com>
+ *
+ * @package heidelpay\php-message-code-mapper\exceptions
+ */
+class MissingLocaleFileException extends Exception
+{
+}
